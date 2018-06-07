7cadd8a6ba56a3009ef163e3459a175375772621
zanku <zanku@i.ua>
2018-06-07 22:33:10 +0300

    add component Operand

--- Angular/Homework_35/Calculator/src/app/app.module.ts
+++ Angular/Homework_35/Calculator/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OperandComponent } from './operand/operand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperandComponent
   ],
   imports: [
     BrowserModule
