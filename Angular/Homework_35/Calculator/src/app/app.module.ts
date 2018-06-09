2ad06e558659fb39e1d8c80df72e458c187f83de
zanku <zanku@i.ua>
2018-06-09 17:05:51 +0300

    add component Result

--- Angular/Homework_35/Calculator/src/app/app.module.ts
+++ Angular/Homework_35/Calculator/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OperandComponent } from './operand/operand.component';
 import { SingComponent } from './sign/sign.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OperandComponent,
-    SingComponent
+    SingComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
