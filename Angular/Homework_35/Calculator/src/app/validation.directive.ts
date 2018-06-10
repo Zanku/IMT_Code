9be9a3fcb183b5c891a7193321997535b9924340
zanku <zanku@i.ua>
2018-06-10 21:55:56 +0300

    add settings of Validation directive

--- Angular/Homework_35/Calculator/src/app/validation.directive.ts
+++ Angular/Homework_35/Calculator/src/app/validation.directive.ts
@@ -1,10 +1,41 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appValidation]'
 })
+
+
+
 export class ValidationDirective {
 
-  constructor() { }
+  constructor( private elem : ElementRef ) {}
 
+  @HostListener("input") onInput(){
+    this.validation();
+  }
+
+  validation(){
+    this.functionDefinition();
+  }
+
+  digitsValidation(){
+    this.elem.nativeElement.value = this.elem.nativeElement.value.replace(/[^0-9]*/g, "");
+  }
+
+  signValidation(){
+    this.elem.nativeElement.value = this.elem.nativeElement.value.replace(/[\w]/g, "");
+  }
+
+  functionDefinition(){
+    switch ( this.elem.nativeElement.placeholder ){
+      case "Sign" :
+        this.signValidation()
+            break;
+      case "Digits" :
+        this.digitsValidation();
+        break;
+      default :
+        console.log("something WRONG");
+    }
+  }
 }
