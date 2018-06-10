0a2c82fa7f00ea734919369c259b6c2e0b7d325d
zanku <zanku@i.ua>
2018-06-11 01:31:20 +0300

    add component Input

--- Angular/Homework_36/ToDoList/src/app/app.module.ts
+++ Angular/Homework_36/ToDoList/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
