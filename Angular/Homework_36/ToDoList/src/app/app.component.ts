ddf23c0ea290fef17e4405502c8356c39e744e7f
zanku <zanku@i.ua>
2018-06-11 04:23:39 +0300

    add features to component Input

--- Angular/Homework_36/ToDoList/src/app/app.component.ts
+++ Angular/Homework_36/ToDoList/src/app/app.component.ts
@@ -9,9 +9,20 @@ export class AppComponent {
 
   list = [
     { name : "Вынести мусор",   status : "Working"},
-    { name : "Помыть посуду",   status : "none"},
-    { name : "Завладеть миром", status : "none"}
+    { name : "Помыть посуду",   status : "none"   },
+    { name : "Завладеть миром", status : "none"   }
   ];
+
+  status = [
+    { condition : "hover",    color : "red"   },
+    { condition : "working",  color : "yellow"},
+    { condition : "done",     color : "green" }
+  ];
+
+
+  addItem( item ){
+    this.list.push( item );
+  }
 
   removeItem( event ){
     this.list = Object.create( this.list.filter( ( item, i  ) => {
--- Angular/Homework_36/ToDoList/src/app/input/input.component.ts
+++ Angular/Homework_36/ToDoList/src/app/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
+  @Input() status;
 
-  constructor() { }
+  @Output() addElement = new EventEmitter();
+
+  element;
+  elemStatus : string;
+  inputValue : string;
+
+  constructor() {
+    this.renew();
+  }
 
   ngOnInit() {
   }
 
+  addElementToList(){
+    this.element = { name : this.inputValue, status : this.elemStatus};
+    this.addElement.emit( this.element );
+    this.renew();
+  }
+
+  renew(){
+    this.inputValue = "";
+    this.elemStatus = "";
+  }
 }
