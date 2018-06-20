dab3916778a46b12cbaa52b68c3bebdf3723583f
zanku <zanku@i.ua>
2018-06-20 09:24:17 +0300

    add features to calc-data service

--- Angular/Homework_38/calc2/src/app/calc-data.service.ts
+++ Angular/Homework_38/calc2/src/app/calc-data.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
+const LENGTH = 5;
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class CalcDataService {
 
+  values : string[];
+   signs : string[];
+  result : number;
+
   constructor() { }
+
+  getValues() : string[] {
+    return this.values;
+  }
+
+  addValue( data : string, id : number ){
+    this.values[ id ] = data;
+  }
+
+  getSigns() : string[] {
+    return this.signs;
+  }
+
+  addSign( data : string, id : number ){
+    this.signs[ id ] = data;
+  }
+
+  getResult() : number {
+    return this.result;
+  }
 }
--- Angular/Homework_38/calc2/src/app/calc/calc.component.ts
+++ Angular/Homework_38/calc2/src/app/calc/calc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CalcDataService} from "../calc-data.service";
 
 @Component({
   selector: 'app-calc',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalcComponent implements OnInit {
 
-  constructor() { }
+  constructor( private calcData : CalcDataService ) { }
 
   ngOnInit() {
   }
 
+  
 }
